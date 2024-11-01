84cf9c584ff02d4b3ae8bb6ba5aee7c1f5d7631f
unknown <andreluisjacobroman@gmail.com>
2024-11-01 15:55:13 -0300

    subindo atividade

--- mobile/lib/service/transacoes_service.dart
+++ mobile/lib/service/transacoes_service.dart
@@ -3,48 +3,36 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:mobile/service/abstract_service.dart';
 
-class TransacoesService extends AbstractService{
-
+class TransacoesService extends AbstractService {
   Future<void> getTransacoes() async {
-    var response = await http.get(Uri.parse("http://localhost:3000/transacoes"));
+    var response =
+        await http.get(Uri.parse("http://localhost:3000/transacoes"));
 
     print("Response:  $response");
     print("Body: ${response.body}");
   }
 
   Future<void> postTransacoes() async {
-    var response = await http.post(Uri.parse("http://localhost:3000/transacoes"),
-     headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode({
-    "id":3,
-    "valor":600.00,
-    "tipo":"Credito"
-    })
-    
-    );
+    var response =
+        await http.post(Uri.parse("http://localhost:3000/transacoes"),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: jsonEncode({"id": 3, "valor": 600.00, "tipo": "Credito"}));
 
-  print("Response: foi");
-
+    print("Response: foi");
   }
 
   Future<void> putTransacoes() async {
-    var response = await http.put(Uri.parse("http://localhost:3000/transacoes/3"),
-  body: jsonEncode({
-    "id":3,
-    "valor": 650.00,
-    "tipo":"Debito"
-  })
+    var response = await http.put(
+        Uri.parse("http://localhost:3000/transacoes/3"),
+        body: jsonEncode({"id": 3, "valor": 650.00, "tipo": "Debito"}));
 
-  );
-
-  print("Response: ${response.body}");
-
+    print("Response: ${response.body}");
   }
-  
+
   @override
   String recurso() {
     return "transacaoes";
   }
-  }
+}
